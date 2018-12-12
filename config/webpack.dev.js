3fe166348adec43c8e46c4f8207e021c23999f81
alexkcollier <alexkcollier@gmail.com>
2018-12-12 14:16:36 -0500

    chore: removes old webpack config [skip ci]

--- config/webpack.dev.js
+++
@@ -1,8 +0,0 @@
-const commonWebpackConfig = require('./webpack.common')
-const merge = require('webpack-merge')
-
-module.exports = merge(commonWebpackConfig, {
-  mode: 'development',
-  watch: true,
-  devtool: 'cheap-module-eval-source-map'
-})
